0df9c11a0b6bc338caddd7cfdc98aa35b2b7e3d6
Michael Telford <Michael.Telford@ibm.com>
2022-05-17 12:37:07 +0100

    Updated mac os x default config

--- macosx/install.sh
+++ macosx/install.sh
@@ -8,32 +8,11 @@
 if test "$(uname)" = "Darwin"; then
 	info "Starting Mac OS X install..."
 
-  # Display system/hidden files in finder. 
-	defaults write com.apple.finder AppleShowAllFiles TRUE
-
-	# Use AirDrop over every interface. Seriously, this should be a default.
-	defaults write com.apple.NetworkBrowser BrowseAllInterfaces 1
+    # Display system/hidden files in finder by default. 
+	defaults write com.apple.finder AppleShowAllFiles true
 
 	# Always open everything in Finder's list view. 
 	defaults write com.apple.Finder FXPreferredViewStyle Nlsv
-
-	# Show the ~/Library folder.
-	chflags nohidden ~/Library
-
-	# Set the Finder prefs for showing a few different volumes on the Desktop.
-	defaults write com.apple.finder ShowExternalHardDrivesOnDesktop -bool true
-	defaults write com.apple.finder ShowRemovableMediaOnDesktop -bool true
-
-	# Run the screensaver if we're in the bottom-right hot corner.
-	defaults write com.apple.dock wvous-br-corner -int 5
-	defaults write com.apple.dock wvous-br-modifier -int 0
-
-	# Set up Safari for development.
-	defaults write com.apple.Safari IncludeInternalDebugMenu -bool true
-	defaults write com.apple.Safari IncludeDevelopMenu -bool true
-	defaults write com.apple.Safari WebKitDeveloperExtrasEnabledPreferenceKey -bool true
-	defaults write com.apple.Safari "com.apple.Safari.ContentPageGroupIdentifier.WebKit2DeveloperExtrasEnabled" -bool true
-	defaults write NSGlobalDomain WebKitDeveloperExtras -bool true
 
 	success "Mac OS X installation complete"
 fi
